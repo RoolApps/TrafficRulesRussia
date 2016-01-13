eed518e4350ca70d82db1bd8724fdf605c2d3de7
Oleg <metallheadnoob@gmail.com>
2016-01-14 02:58:39 +0300

    Added initial application logic

---
+++ Tickets/AppLogic/IAnswer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogic
+{
+    public interface IAnswer
+    {
+        int Id { get; set; }
+        bool IsRight { get; set; }
+        String Text { get; set; }
+    }
+}
---
+++ Tickets/AppLogic/IDataAccessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogic
+{
+    public interface IDataAccessor
+    {
+        int[] GetTicketNums();
+
+        int[] GetQuestionsByTickets(int[] tickets);
+
+        IQuestion GetQuestionById(int id);
+    }
+}
