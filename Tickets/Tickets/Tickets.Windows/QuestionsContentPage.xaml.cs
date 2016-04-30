f0401b990e7634251c3c0d1c7d251f892e1b5fac
Febonachi <lol131@mail.ru>
2016-04-30 19:39:54 +0300

    Story(Mark selected answers) and some refactoring for other xaml files

--- Tickets/Tickets/Tickets.Windows/QuestionsContentPage.xaml.cs
+++ Tickets/Tickets/Tickets.Windows/QuestionsContentPage.xaml.cs
@@ -18,24 +18,43 @@ using AppLogic;
 using AppLogic.Enums;
 using AppLogic.Interfaces;
 using Tickets;
+using Windows.UI;
 
 namespace Tickets
 {
     public sealed partial class QuestionsContentPage : Page
     {
-        public QuestionsContentPage()
-        {
+        public QuestionsContentPage() {
             this.InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
             ISession session = e.Parameter as ISession;
             PagedCanvas paged_canvas = flipping_canvas.Children.OfType<PagedCanvas>().Single();
             PagedCollection<IQuestion> paged_col = new PagedCollection<IQuestion>(2);
             paged_col.DataSource = session.Questions;
             paged_canvas.ItemsSource = paged_col;
         }
-        
+
+        private void Grid_Tapped(object sender, TappedRoutedEventArgs e) {
+            TextBlock tb = e.OriginalSource as TextBlock;
+            if (tb != null) {
+                IAnswer answer = ((tb).DataContext) as IAnswer;
+                answer.IsSelected = !answer.IsSelected;
+            }
+        }
+    }
+
+    public class BorderBackgroundColorConverter : IValueConverter {
+        const String Selected = "Gray";
+        const String NotSelected = "Black";
+
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            return (bool)value ? Selected : NotSelected;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            throw new NotImplementedException();
+        }
     }
 }
