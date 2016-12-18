315a280a4455353be47f6a6f5d5de2c8547c2358
Geslot <devgeslot@gmail.com>
2016-12-18 17:22:54 +0300

    Added static content load

---
+++ Tickets/AppLogic/Static/PreloadedContent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLiteShared.Models;
+
+namespace AppLogic.Static
+{
+    public static class PreloadedContent
+    {
+        public static PreloadedTable<Chapters> RuleChapters;
+        public static PreloadedTable<Signs> Signs;
+        public static PreloadedTable<Marks> Marks;
+
+        public static void LoadData()
+        {
+            RuleChapters = new PreloadedTable<Chapters>();
+            Signs = new PreloadedTable<Signs>();
+            Marks = new PreloadedTable<Marks>();
+        }
+    }
+}
