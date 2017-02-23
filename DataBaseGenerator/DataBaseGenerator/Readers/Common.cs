e41fd984d2b96c61102b039998d46b2acb8ec065
Geslot <devgeslot@gmail.com>
2017-02-23 15:03:03 +0300

    Generate rules from other site

--- DataBaseGenerator/DataBaseGenerator/Readers/Common.cs
+++ DataBaseGenerator/DataBaseGenerator/Readers/Common.cs
@@ -20,7 +20,7 @@ namespace DataBaseGenerator.Readers
         public static String ParseNode(CsQuery.IDomObject node, int level = 0)
         {
             var result = String.Empty;
-            if (node.NodeName == NodeTypes.Text && (node.NodeValue ?? String.Empty) != String.Empty)
+            if (node.NodeName == NodeTypes.Text && (node.NodeValue ?? String.Empty).Trim() != String.Empty)
             {
                 result = String.Format("<Run>{0}</Run>", node.NodeValue);
             }
@@ -36,7 +36,7 @@ namespace DataBaseGenerator.Readers
                 var pattern = @"<Paragraph xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"">{0}</Paragraph>";
                 if (node.ChildNodes.Any())
                 {
-                    var content = String.Join("", node.ChildNodes.Select(childNode => ParseNode(childNode, level + 1)));
+                    var content = String.Join(GetIdent(level + 1), node.ChildNodes.Select(childNode => ParseNode(childNode, level + 1)));
                     result = String.Format(pattern, content);
                 }
                 else
@@ -44,12 +44,12 @@ namespace DataBaseGenerator.Readers
                     result = String.Format(pattern, node.NodeValue);
                 }
             }
-            if (node.NodeName == NodeTypes.Bold)
+            if (new String[] { NodeTypes.Bold, NodeTypes.Italic }.Contains(node.NodeName))
             {
-                var pattern = "<Bold>{0}</Bold>";
+                var pattern = PatternFactory.Get(node.NodeName);
                 if (node.ChildNodes.Any())
                 {
-                    var content = String.Join("", node.ChildNodes.Select(childNode => ParseNode(childNode, level + 1)));
+                    var content = String.Join(GetIdent(level + 1), node.ChildNodes.Select(childNode => ParseNode(childNode, level + 1)));
                     result = String.Format(pattern, content);
                 }
                 else
@@ -57,27 +57,45 @@ namespace DataBaseGenerator.Readers
                     result = String.Format(pattern, node.NodeValue);
                 }
             }
-            if (node.NodeName == NodeTypes.Font)
+            if (new String[] { NodeTypes.Font, NodeTypes.Span }.Contains(node.NodeName))
             {
-                if (node.ChildNodes.Any())
+                if(node.Classes.Contains("pdd-inline-sign"))
                 {
-                    result = String.Join("", node.ChildNodes.Select(childNode => ParseNode(childNode, level + 1)));
+                    var match = new Regex(@"https://media.am.ru/pdd/(?<type>\w+)/xs/(?<num>[\w\.]+).png").Match(node.InnerHTML);
+                    if(match != null)
+                    {
+                        var type = match.Groups["type"].Value;
+                        var num = match.Groups["num"].Value;
+                        if(type == "marking")
+                        {
+                            type = "mark";
+                        }
+                        return String.Format("<Run> @@{{{0}s}}{1}@@ </Run>", match.Groups["type"].Value, match.Groups["num"].Value);
+                    }
+                }
+                else if (node.ChildNodes.Any())
+                {
+                    result = String.Join(GetIdent(level + 1), node.ChildNodes.Select(childNode => ParseNode(childNode, level + 1)));
                 }
             }
-            if (node.NodeName == NodeTypes.Link)
+            if(node.NodeName == NodeTypes.List)
             {
-                var ruleObjects = new string[] { "signs", "marks" };
-                if (ruleObjects.Any(ruleObject => (node.GetAttribute("href") ?? String.Empty).Contains(ruleObject)))
-                {
-                    result = String.Format("<Run>{0}</Run>", ReplaceUrl(node.ChildNodes.Single(chileNode => chileNode.NodeName == NodeTypes.Text).NodeValue,
-                        ruleObjects.Single(ruleObject => node.GetAttribute("href").Contains(ruleObject))));
-                }
+                result = String.Join(Environment.NewLine, node.ChildNodes.Select(childNode => ParseNode(childNode, level + 1)));
+            }
+            if(node.NodeName == NodeTypes.ListItem)
+            {
+                result = String.Join(GetIdent(level + 1), node.ChildNodes.Select(childNode => "<Run>-</Run>" + ParseNode(childNode, level + 1)));
             }
             if (level == 0 && result != String.Empty && !(new string[] { NodeTypes.Paragraph, NodeTypes.Header }).Contains(node.NodeName))
             {
                 result = String.Format(@"<Paragraph xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"">{0}</Paragraph>", result);
             }
             return result;
+        }
+
+        static string GetIdent(int level)
+        {
+            return Environment.NewLine + new String(Enumerable.Range(0, level * 2).Select(i => ' ').ToArray());
         }
 
         static string ReplaceUrl(string text, string replace)
@@ -99,12 +117,37 @@ namespace DataBaseGenerator.Readers
 
     public static class NodeTypes
     {
-        public const String Header = "H4";
+        public const String Header = "H2";
         public const String Paragraph = "P";
         public const String Link = "A";
         public const String Bold = "STRONG";
         public const String Text = "#text";
         public const String Td = "TD";
         public const String Font = "FONT";
+        public const String Span = "SPAN";
+        public const String Italic = "EM";
+        public const String List = "UL";
+        public const String ListItem = "LI";
+    }
+
+    public static class PatternFactory
+    {
+        private static Dictionary<String, String> values = new Dictionary<String, String>()
+        {
+            { NodeTypes.Bold, "<Bold>{0}</Bold>" },
+            { NodeTypes.Italic, "<Italic>{0}</Italic>"}
+        };
+
+        public static String Get(String nodeType)
+        {
+            if(values.ContainsKey(nodeType))
+            {
+                return values[nodeType];
+            }
+            else
+            {
+                return "{0}";
+            }
+        }
     }
 }
